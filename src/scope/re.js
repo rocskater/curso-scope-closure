0285f9a9a06c11a9a51898242a672343c688765a
rocskater <rocskater@gmail.com>
2022-08-17 15:00:30 -0500

    ya pa salir

--- src/scope/re.js
+++ src/scope/re.js
@@ -12,3 +12,13 @@ var secondName = 'David';
 var secondName = 'Ana';
 
 console.log(secondName);
+
+let fruit = 'Apple';
+fruit ='Kiwi';
+
+const  vehicles =[];
+vehicles.push("🚑");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
